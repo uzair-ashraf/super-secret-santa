782fd3f83687ca96cf7cba44fd5b75553713a250
Uzair Ashraf <uzinatorcl@live.com>
2020-11-02 21:18:02 -0800

    serializing save data

--- main.py
+++ main.py
@@ -7,12 +7,56 @@ from user import User
 
 secret_santa_list = []
 players = []
+show_selected_users = False
+print('Starting Super Secret Santa!')
+try:
+  print('Attempting to find data')
+  with open('./data.json') as data:
+    user_data = json.load(data)
+  print('We have discovered preexisting data with the following users.')
 
-with open('./data.json') as data:
-  user_data = json.load(data)
   for user in user_data:
-    secret_santa_list.append(User(user['name'], user['email']))
-    players.append(user['name'])
+    print(user['name'])
+
+  print('Would you like to run super secret santa with these players?')
+  possible_answers = ['y', 'n']
+  continue_with_old_users = input("Please input y or n to continue: ")
+  while continue_with_old_users not in possible_answers:
+    continue_with_old_users = input("Unrecognized command, please enter y or n: ")
+
+  print('Understood!')
+  if continue_with_old_users == 'y':
+    for user in user_data:
+      secret_santa_list.append(User(user['name'], user['email']))
+      players.append(user['name'])
+  else:
+    user_input = ""
+    while True:
+      user_input = input('Please enter each players first name and email separated by spaces.\nOnce you are done, please type done. \n')
+      if user_input == 'done':
+        break
+      secret_santa_list.append(User(*user_input.split(' ')))
+    print(secret_santa_list)
+    for user in secret_santa_list:
+      players.append(user.name)
+
+    print('Success! We have created entries for the following players: ')
+    for user in secret_santa_list:
+      print(user.name +
+            ' with the email ' +
+            user.email)
+    print('Would you like to overwrite your previous save file with the new users?')
+    possible_answers = ['y', 'n']
+    overwrite_save_data = input("Please input y or n to overwrite: ")
+    while overwrite_save_data not in possible_answers:
+      overwrite_save_data = input("Unrecognized command, please enter y or n: ")
+    if overwrite_save_data == 'y':
+      with open('./data.json') as save_data:
+        save_data.write(secret_santa.serialize_users(secret_santa_list))
+
+except FileNotFoundError:
+  print('oopsie')
+
 
 
 secret_santa.assign_users_their_secret_santas(secret_santa_list, players)
@@ -22,10 +66,10 @@ password = getpass('Please enter your password: ')
 
 yag = yagmail.SMTP(email, password)
 
-for user in secret_santa_list:
-  yag.send(user.email, 'Secret Santa Selected!', [
-      'Ho ho ho!',
-      'Be ready for the anxiety ridden shopping of buying something that you hope this person will like,',
-      'The person that was selected for you was ' + user.selected_secret_santa,
-      'Good luck! Remember I have no idea who was selected for you, it is deleted immediately for me aftewards.'
-  ])
+# for user in secret_santa_list:
+#   yag.send(user.email, 'Secret Santa Selected!', [
+#       'Ho ho ho!',
+#       'Be ready for the anxiety ridden shopping of buying something that you hope this person will like,',
+#       'The person that was selected for you was ' + user.selected_secret_santa,
+#       'Good luck! Remember I have no idea who was selected for you, it is deleted immediately for me aftewards.'
+#   ])
--- secret_santa.py
+++ secret_santa.py
@@ -1,4 +1,5 @@
 import random
+import json
 
 def assign_users_their_secret_santas(users, players):
   random.shuffle(users)
@@ -7,7 +8,16 @@ def assign_users_their_secret_santas(users, players):
     selected_santa = user.name
     selected_player = random.choice(players)
     # Just incase the random player we selected was the same as the secret santa
-    while(selected_santa == selected_player):
+    while selected_santa == selected_player:
       selected_player = random.choice(players)
     user.set_selected_secret_santa(selected_player)
     players.remove(selected_player)
+
+def serialize_users(users):
+  result = []
+  for user in users:
+    result.append({
+      'name': user.name,
+      'email': user.email
+    })
+  return json.dumps(result)
